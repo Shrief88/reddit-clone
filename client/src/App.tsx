4400393bd13ff27c94fc265d3ddd839378aa4c5c
Shrief Essam <shriefessam1999@gmail.com>
2024-01-14 15:56:33 +0200

    add basic packages for frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/button"
+
+function App() {
+  return (
+   <div className="text-blue-500"><Button variant={"outline"}>Button</Button></div>
+  )
+}
+
+export default App
