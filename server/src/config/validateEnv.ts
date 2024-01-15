909ddf02bfb07591ad6d3fb0a14a9d90626a3ab1
Shrief Essam <shriefessam1999@gmail.com>
2024-01-16 01:34:23 +0200

    connect with database using prisma

--- server/src/config/validateEnv.ts
+++ server/src/config/validateEnv.ts
@@ -7,6 +7,7 @@ const env = cleanEnv(process.env, {
   PORT: port(),
   NODE_ENV: str({ choices: ["development", "test", "production"] }),
   BASE_URL: str(),
+  DATABASE_URL: str(),
 });
 
 export default env;
