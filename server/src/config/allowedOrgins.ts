1919c8da1044457be451ad74375258020fff7918
Shrief Essam <shriefessam1999@gmail.com>
2024-02-13 20:08:02 +0200

    add vercel site to allowed orgins

--- server/src/config/allowedOrgins.ts
+++ server/src/config/allowedOrgins.ts
@@ -1,3 +1,6 @@
-const allowedOrgins = ["http://localhost:5173"];
+const allowedOrgins = [
+  "http://localhost:5173",
+  "https://reddit-clone-alpha-snowy.vercel.app",
+];
 
 export default allowedOrgins;
