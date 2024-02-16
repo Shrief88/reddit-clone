1404e62d9088e618af779e8b834030332c4d141b
Shrief Essam <shriefessam1999@gmail.com>
2024-02-16 12:18:42 +0200

    add loading state when app is starting

--- client/src/components/layout/RequireAuth.tsx
+++ client/src/components/layout/RequireAuth.tsx
@@ -1,11 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "@/hooks/useAuth";
+import { Loader2 } from "lucide-react";
 
 const RequireAuth = () => {
   const { user, isLoading } = useAuth();
 
   if (isLoading) {
-    return null;
+    return (
+      <div className="flex items-center justify-center">
+        <Loader2 className="mx-auto animate-spin"/>
+      </div>
+    );
   }
 
   return user ? <Outlet /> : <Navigate to="/login" replace={true} />;
